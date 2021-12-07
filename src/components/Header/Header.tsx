cb9bba6e11d5a363475f5159e515bca421800453
nojipiz <nojipiz@protonmail.com>
2021-12-07 22:49:53 +0000

    Header structured

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import styles from './styles.module.css'
+import { MdEmail } from 'react-icons/md'
 
 class Header extends Component {
   render() {
     return (
       <header className={styles.header}>
         <h1> Pepe </h1>
-        <div className={styles.buttonsWrapper}>
-          <button>Sobre mi</button>
-          <button>Portafolio</button>
-          <button>Escribeme</button>
-        </div>
+
+        <nav className={styles.navWrapper}>
+          <ul className={styles.listWrapper}>
+            <li className={styles.commonButton}>Sobre mi</li>
+            <li className={styles.commonButton}>Porfolio</li>
+            <li className={styles.writeMe}>Escribeme <MdEmail /></li>
+          </ul>
+        </nav>
       </header>
     )
   }
