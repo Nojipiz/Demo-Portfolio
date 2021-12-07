e4bf3bfd6aa96707f92092e2845f0ec8adb4445f
nojipiz <nojipiz@protonmail.com>
2021-12-07 23:08:39 +0000

    Posted about me photo<

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import styles from './styles.module.css'
 
 class AboutMe extends Component {
   render() {
     return (
-      <div>
+      <section className={styles.mainSection}>
+        <div className={styles.contentWrapper}>
+          <img src="photo-me.png" />
+          <div className={styles.aboutMeWrapper}>
 
-      </div>
+          </div>
+        </div>
+      </section>
     )
   }
 }
